00837c766c01eb301e478d2b45359a0f6eaf66af
Ryan Dunphy <rbdunphy7@gmail.com>
2022-04-04 11:33:46 -0700

    lab2 problem 1 complete and tested, added launch.json dynamic path for debug

--- Lab2/string.py
+++ Lab2/string.py
@@ -1,1 +1,15 @@
 # Write a python function called string_rev to reverse a string.
+
+def string_rev(inputStr):
+    outputStr = ''
+    for i in range(len(inputStr)):
+        idx = len(inputStr) - i 
+        outputStr = outputStr + inputStr[idx-1]
+    print("Input string: " + inputStr + "\n")
+    print("Output string: " + outputStr + "\n")
+
+# Prompt user for string input
+strInput = input("Enter string to reverse: \n")
+print("** Reversing **")
+print("***************")
+string_rev(strInput)
